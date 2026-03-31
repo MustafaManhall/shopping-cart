3ddfc0ab408b72589e93991f9c7ca82a60d4b2a8
Mustafa Manhal <100897710+MustafaManhall@users.noreply.github.com>
2026-04-01 02:35:21 +0300

    fix: CORS issue
    - resolve CORS issue by fetching products server-side in Next.js.
    - adding a error file to the shop page.

---
+++ app/shop/error.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function ComponentName () {
+  return (
+    <div className="flex justify-center items-center text-2xl">
+      <h1>Failed to load products(code 523). Please try again later.</h1>
+    </div>
+  );
+}
--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,26 +1,10 @@
-"use client";
-
 import ProductCard from "@/components/productCard";
 import { fetchProducts } from "@/lib/fetchProducts";
-import { Product } from "@/types/product";
-import { useEffect, useState } from "react";
 
-export default function Shop() {
-  const [shopData, setShopData] = useState<Product[]>([]);
+export default async function Shop() {
+  const shopData = await fetchProducts();
   const categories = [...new Set(shopData.map((item) => item.category))];
-
-  useEffect(() => {
-    async function fetchingData() {
-      try {
-        const data = await fetchProducts();
-        setShopData(data);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    fetchingData();
-  }, []);
-
+  
   return (
     <main className="flex flex-col justify-center items-center">
       <div className="flex flex-col gap-4 items-center mb-4">
